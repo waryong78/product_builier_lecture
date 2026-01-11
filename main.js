3483685d7bcedc4def06ac67f81353886f4fcc15
waryong78 <waryong78@gmail.com>
2026-01-11 02:40:30 +0000

    fix: update broken image links

--- main.js
+++ main.js
@@ -10,18 +10,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const html = document.documentElement;
 
     const menus = [
-        { category: 'Korean', name: 'Bibimbap', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Bibimbap_mixed.jpg/1000px-Bibimbap_mixed.jpg' },
-        { category: 'Korean', name: 'Kimchi Jjigae', image: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Korean_stew-Kimchi_jjigae-01.jpg' },
-        { category: 'Korean', name: 'Bulgogi', image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Bulgogi.JPG' },
-        { category: 'Japanese', name: 'Sushi', image: 'https://upload.wikimedia.org/wikipedia/commons/4/46/Sushi_%284%29.jpg' },
-        { category: 'Japanese', name: 'Ramen', image: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Ramen_Jump_002.jpg' },
-        { category: 'Japanese', name: 'Tonkatsu', image: 'https://upload.wikimedia.org/wikipedia/commons/T/To/Tonkatsu_Plate.jpg' },
-        { category: 'Chinese', name: 'Jajangmyeon', image: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/Korean_black_bean_noodle_dish-Jaengban_Jajangmyeon-01.jpg' },
-        { category: 'Chinese', name: 'Mapo Tofu', image: 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Mapo_Tofu_%288840681150%29.jpg' },
-        { category: 'Chinese', name: 'Sweet and Sour Pork', image: 'https://upload.wikimedia.org/wikipedia/commons/a/a2/Flickr_-_Sweet_and_Sour_Pork.jpg' },
+        { category: 'Korean', name: 'Bibimbap', image: 'https://plus.unsplash.com/premium_photo-1664472660721-5524c5123919?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+        { category: 'Korean', name: 'Kimchi Jjigae', image: 'https://images.unsplash.com/photo-1586523587524-175835933391?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+        { category: 'Korean', name: 'Bulgogi', image: 'https://images.unsplash.com/photo-1593562280925-a0a4c284b3dc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+        { category: 'Japanese', name: 'Sushi', image: 'https://images.unsplash.com/photo-1579871494447-9811cf80d66c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+        { category: 'Japanese', name: 'Ramen', image: 'https://images.unsplash.com/photo-1557872943-16a5ac26437e?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+        { category: 'Japanese', name: 'Tonkatsu', image: 'https://images.unsplash.com/photo-1588631115379-b1b85099a9a3?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+        { category: 'Chinese', name: 'Jajangmyeon', image: 'https://images.unsplash.com/photo-1626082270921-b9979bca3a62?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+        { category: 'Chinese', name: 'Mapo Tofu', image: 'https://images.unsplash.com/photo-1606114382348-5ac58a9ab44d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+        { category: 'Chinese', name: 'Sweet and Sour Pork', image: 'https://images.unsplash.com/photo-1522861234477-0c7cb35b3838?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
         { category: 'Western', name: 'Pizza', image: 'https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-        { category: 'Western', name: 'Pasta', image: 'https://images.unsplash.com/photo-1551887373-632b7b192648?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-        { category: 'Western', name: 'Steak', image: 'https://upload.wikimedia.org/wikipedia/commons/e/e4/Steak-Valladolid-2009.jpg' },
+        { category: 'Western', name: 'Pasta', image: 'https://images.unsplash.com/photo-1621996346565-e326b20f545c?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+        { category: 'Western', name: 'Steak', image: 'https://images.unsplash.com/photo-1529692236671-f1f6cf9683ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
     ];
 
     const recommendMenu = () => {
